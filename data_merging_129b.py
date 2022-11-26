ffb3c30b2c323c1eca5714ed4f6766a12fdb675f
ashenthan <68573593+ashenthan@users.noreply.github.com>
2022-11-26 12:23:54 -0500

    Add files via upload

---
+++ data_merging_129b.py
@@ -0,0 +1,45 @@
+
+import csv
+import pandas as pd
+
+#put the file names in the 2 variables as below:-
+file1 = 'bright_stars.csv'
+file2 = 'unit_converted_stars.csv'
+# declare 2 empty lists d1 and d2
+d1=[]
+d2=[]
+with open(file1,'r',encoding='utf8') as f:
+    csv_reader =csv.reader(f)
+    
+    for i in csv_reader:
+        d1.append(i)
+        
+with open(file2,'r',encoding='utf8') as f:
+    csv_reader = csv.reader(f)
+    
+    for i in csv_reader:
+        d2.append(i)
+#pick header from d1[0] and put to h1 ref h2 and make it same for h1
+h1=d1[0]
+h2 = d2[0]
+
+
+#pick planet data from d1[1:] and assign to p_d1 refer to p_d2
+p_d1=d1[1:]
+p_d2 = d2[1:]
+#add h1 and h2 and put in variable h
+h=h1+h2
+
+p_d =[]
+
+for i in p_d1:
+    p_d.append(i)
+for j in p_d2:
+    p_d.append(j)
+with open("total_stars.csv",'w',encoding='utf8') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(h)   
+    csvwriter.writerows(p_d)
+    
+df = pd.read_csv('total_stars.csv')
+df.tail(8)
